aae3a04ca3d4a1d06a105621d1873eacdd799217
Alon <alon1853@gmail.com>
2017-03-27 19:50:07 +0300

    Add partition to the simulated topic

--- src/server/kafka_client.js
+++ src/server/kafka_client.js
@@ -2,19 +2,16 @@ const kafkaRest = require('kafka-rest');
 const KAFKA_HOST = process.env.KAFKA_REST_PROXY_IP || 'localhost';
 const KAFKA_PORT = process.env.KAFKA_REST_PROXY_PORT || 8082;
 
-const fs = require('fs');
-
 const kafka = new kafkaRest({ 'url': 'http://' + KAFKA_HOST + ':' + KAFKA_PORT });
 
-
 function SimulateAvro() {
-  kafka.topic('test2').produce('message', (err, res) => {
-    if (!err) {
-      console.log('Created topic test2 successfully!');
-    } else {
-      console.log(err);
-    }
-  });
+  // kafka.topic('test2').produce('message', (err, res) => {
+  //   if (!err) {
+  //     console.log('Created topic test2 successfully!');
+  //   } else {
+  //     console.log(err);
+  //   }
+  // });
 
   const schema = new kafkaRest.AvroSchema({
     "name": "TweetText",
@@ -24,7 +21,7 @@ function SimulateAvro() {
       { "name": "text", "type": "string" }
     ]
   });
-  const target = kafka.topic('my-topic');
+  const target = kafka.topic('my-topic').partition(0);
   const saved_data = {
     'id': '1',
     'text': 'My tweet'
