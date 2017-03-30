b842114615f69272dd475aba57e04637f7d3bdf9
Alon <alon1853@gmail.com>
2017-03-30 16:08:54 +0300

    Try to fix client socket

--- src/client/socket_client.js
+++ src/client/socket_client.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socketClient = io();
+const socketClient = io('http://77.138.42.10:9090/api/v1/proxy/namespaces/default/services/ui-best-service');
 
 socketClient.on('connect', () => {
 	console.log('Socket is alive! (' + new Date() + ')');
